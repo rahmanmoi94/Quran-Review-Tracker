48bcedef5dee79b74fa7978531888a8cdb1a2eeb
rahmanmoi94 <rahmanmoi94@gmail.com>
2026-07-06 16:55:47 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.QURAN_TRACKER_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://ywppaifmqayfkuywcmww.supabase.co",
+  supabaseAnonKey: "sb_publishable_Ji9OWCDOiBLz2VIKF-hMeQ_qYE9uyEJ",
 };
